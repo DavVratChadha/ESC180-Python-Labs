704b9ecba47df9c6537d5b38eaa67e2ca298bc07
DavVratChadha <95514650+DavVratChadha@users.noreply.github.com>
2021-12-04 12:25:23 -0500

    Add files via upload

---
+++ Lab 1/Hello.py
@@ -0,0 +1,32 @@
+#LAB1
+#By Dav Vrat Chadha and Mehul Bhardwaj
+
+#Problem 1
+print("Hello, Python")
+print()
+
+#Problem 2
+print("Hello, Dav Vrat Chadha")
+print("Hello, Mehul Bhardwaj")
+print()
+
+#Problem 3
+person1 = "Dav Vrat Chadha"
+person2 = "Mehul Bhardwaj"
+print("Hello, " + person1 + " and " + person2 + ". Your names are " + person2 + " and " + person1 + ". Hi there. Your names are still " + person1 + " and " + person2 + ".")
+print()
+
+#Hello, Hermione Granger and Harry Potter. Your names are Harry Potter and Hermione Granger. Hi there. Your names are still Hermione Granger and Harry Potter.
+
+#Problem 4
+person1 = "Prof. Cluett"
+person2 = "Prof. Thywissen"
+print("Hello, " + person1 + " and " + person2 + ". Your names are " + person2 + " and " + person1 + ". Hi there. Your names are still " + person1 + " and " + person2 + ".")
+print()
+
+#Problem 5
+greetee = "Lord Voldemort"
+if greetee == "Lord Voldemort":
+    print("I'm not talking to you.")
+else:
+    print("Hi " + greetee + "! Nice to meet you!")
